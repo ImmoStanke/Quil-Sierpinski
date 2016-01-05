7d99716f4d7b9eecfa6cf65cdc8b2e8ad8f23632
Immo Stanke <istanke@thoughtworks.com>
2016-01-05 21:10:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# sierpinski
+
+A Quil sketch of a zooming Sierpinski Triangle (
+
+## Usage
+
+LightTable - open `core.clj` and press `Ctrl+Shift+Enter` to evaluate the file.
+
+Emacs - run cider, open `core.clj` and press `C-c C-k` to evaluate the file.
+
+REPL - run `(require 'sierpinski.core)`.
+
+## License
+
+Copyright © 2014
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/sierpinski/core.clj
@@ -0,0 +1,61 @@
+(ns sierpinski.core
+  (:require [quil.core :as q]
+            [quil.middleware :as m]))
+
+(defn setup []
+  (q/frame-rate 30)
+  {:tri-size 1})
+
+(defn update-state [state]
+  {:tri-size (mod (* (:tri-size state) 1.01) 5000)}
+  ;{:tri-size 50}
+  )
+
+(defn get-triangle [size xmiddle ymiddle]
+  [[xmiddle (- ymiddle size)]
+   [(- xmiddle size) (+ ymiddle size)]
+   [(+ xmiddle size) (+ ymiddle size)]])
+
+(defn line-middle
+  [a b] (map #(/ (+ %1 %2) 2) a b)
+  )
+
+(defn draw-nested-triangle [n old-coordinates]
+  (if (> n 0)
+
+    (let [new-coords (map line-middle old-coordinates (rest (take 4 (cycle old-coordinates))))
+          left-tri [(first new-coords) (second old-coordinates) (second new-coords)]
+          mid-tri [(first old-coordinates) (first new-coords) (last new-coords)]
+          right-tri [(last new-coords) (second new-coords) (last old-coordinates)]
+          ]
+
+      (apply q/triangle (apply concat old-coordinates))
+
+      (draw-nested-triangle (- n 1) left-tri)
+      (draw-nested-triangle (- n 1) mid-tri)
+      (recur (- n 1) right-tri)
+
+      )
+    )
+  )
+
+(defn draw-state [state]
+  (q/background 255)
+  (q/fill 255 255)
+  (draw-nested-triangle 12 (get-triangle (:tri-size state) 250 250))
+  )
+
+
+(q/defsketch sierpinski
+             :title "Zooming Sierpinsky Triangle"
+             :size [500 500]
+             ; setup function called only once, during sketch initialization.
+             :setup setup
+             ; update-state is called on each iteration before draw-state.
+             :update update-state
+             :draw draw-state
+             :features [:keep-on-top]
+             ; This sketch uses functional-mode middleware.
+             ; Check quil wiki for more info about middlewares and particularly
+             ; fun-mode.
+             :middleware [m/fun-mode])
